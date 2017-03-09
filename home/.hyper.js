ea9b952606277b4c63f4743c8f53368155268d0f
Tyler Sticka <tyler@tylersticka.com>
2017-03-09 13:26:24 -0800

    Misc. tweaks to Hyper config and prompt

--- home/.hyper.js
+++ home/.hyper.js
@@ -6,27 +6,6 @@ module.exports = {
     // font family with optional fallbacks
     fontFamily: '"Fira Mono OT", "Fira Code", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
 
-    // terminal cursor background color (hex)
-    // cursorColor: '#BBBBBB',
-
-    // color of the text
-    // foregroundColor: '#BBBBBB',
-
-    // terminal background color
-    // backgroundColor: '#DD131313',
-
-    // border color (window, tabs)
-    // borderColor: '#000',
-
-    // custom css to embed in the main window
-    css: '',
-
-    // custom padding (css format, i.e.: `top right bottom left`)
-    termCSS: '',
-
-    // custom padding
-    padding: '12px 14px',
-
     // custom initial window size
     windowSize: [800, 450],
 
@@ -36,27 +15,6 @@ module.exports = {
         color: inherit;
       }
     `
-
-    // some color overrides. see http://bit.ly/29k1iU2 for
-    // the full list
-    // colors: [
-    //   '#121212',
-    //   '#FA2573',
-    //   '#98E123',
-    //   '#DFD460',
-    //   '#1080D0',
-    //   '#6338B3',
-    //   '#43A8D0',
-    //   '#BBBBBB',
-    //   '#555555',
-    //   '#F6669D',
-    //   '#B1E05F',
-    //   '#FFF26D',
-    //   '#00AFFF',
-    //   '#AF87FF',
-    //   '#51CEFF',
-    //   '#ffffff'
-    // ]
   },
 
   // a list of plugins to fetch and install from npm
