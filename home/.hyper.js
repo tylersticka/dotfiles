6899a95cdeb66ee1ac6f025d0a79799a35db1ba1
Tyler Sticka <tyler@tylersticka.com>
2017-04-26 12:55:46 -0700

    Old changes I forgot to commit

--- home/.hyper.js
+++ home/.hyper.js
@@ -17,23 +17,13 @@ module.exports = {
     `
   },
 
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hypersolar`
-  //   `@company/project`
-  //   `project#1.0.1`
   plugins: [
     'hypercwd',
     'hyperlinks',
     'hyperterm-1password',
     'hyperterm-atom-dark',
     'hyperterm-paste',
+    // 'hyper-alt-click',
     'hyperterm-tab-icons'
-  ],
-
-  // in development, you can create a directory under
-  // `~/.hyperterm_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
-  localPlugins: []
+  ]
 };
